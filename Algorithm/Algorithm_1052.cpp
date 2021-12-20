2efbeb246f34d4469e731008a326f9801f94574a
ParkSoonyoung <gmb7000@naver.com>
2021-12-20 11:12:03 +0900

    Update Algorithm_1052.cpp
    1052 add

--- Algorithm/Algorithm_1052.cpp
+++ Algorithm/Algorithm_1052.cpp
@@ -3,9 +3,42 @@
 using namespace std;
 
 
+int Algorithm_1052(int N, int K);
+
+
+int Algorithm_1052(int N, int K)
+{
+    int result = 0;
+    while (1)
+    {
+        int nTemp = N;
+        int cnt = 0;
+        while (nTemp)
+        {
+            if (nTemp % 2 == 1)
+                cnt++;
+
+            nTemp /= 2;
+        }
+
+        if (cnt <= K)
+            return result;
+
+        N++;
+        result++;
+
+    }
+
+}
+
 int main(void)
 {
-	cout << "1052";
+	int N, K;
+    int result;
+	cin >> N >> K;
+    result = Algorithm_1052(N, K);
+
+    cout << result;
 
 
 
