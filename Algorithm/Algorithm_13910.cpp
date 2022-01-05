0f442b05900a7073255d98f08acdaa247055e109
ParkSoonyoung <gmb7000@naver.com>
2022-01-05 15:57:42 +0900

    Update Algorithm_13910.cpp

--- Algorithm/Algorithm_13910.cpp
+++ Algorithm/Algorithm_13910.cpp
@@ -1,12 +1,38 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
 
 
 int main(void)
 {
 
+	int N, M;
+	vector <int>v;
+
+
+	cin >> N >> M;
+
+
+	for (int i = 0; i < M; i++)
+	{
+		int Input;
+		cin >> Input;
+		v.push_back(Input);
+
+	}
+
+	sort(v.begin(), v.end());
+
+
+
+
+
+
+
 
 	return 0;
+
 
 
 }
